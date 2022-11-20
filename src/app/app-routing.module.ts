a7a2947dcdb439523d8faf32935822b841cf559d
ksulao <ksulao@ttu.ee>
2022-11-20 20:57:35 +0200

    requeset user data by id from server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,12 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { IndexComponent } from './index/index.component';
 import { RegisterComponent } from './register/register.component';
-import {ProjectsComponent} from "./projects/projects.component";
-import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'home', component:HomeComponent},
-  {path: '', component: LoginComponent},
-  {path: 'projects', component: ProjectsComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'index', component: IndexComponent },
+  { path: '', component: LoginComponent },
 ];
 
 @NgModule({
@@ -20,11 +17,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule {
   constructor(private _router: Router) {}
-
-  navigateToLogin() {
-    this._router.navigate(['login']);
-  }
-  navigateToHome() {
-    this._router.navigate(['home']);
-  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,10 @@
-import {Component} from "@angular/core";
-import {Employee} from "../model/employee";
-import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  allEmployees: Employee[] = [];
-  constructor(private http: HttpClient, private _router: Router) { }
-
-  ngOnInit(): void {
-    this.fetchEmployees();
-  }
-
-  onEmployeeFetch() {
-    this.fetchEmployees();
-  }
-
-  private fetchEmployees() {
-    this.http.get<Employee>('/api/employee/1')
-      .subscribe((res) => {
-        console.log(res)
-        this.allEmployees = [res]
-      })
-  }
-  navigateToProjects() {
-    this._router.navigate(['projects'])
-  }
-
-  navigateToHome() {
-    this._router.navigate(['home'])
-
-  }
+  ngOnInit(): void {}
 }
