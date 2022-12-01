1bdfb68e92c6f11568e8db8665069c374e2e1d26
ksulao <ksulao@ttu.ee>
2022-12-01 10:59:40 +0200

    project view iimplemented with task creation

--- src/app/project-view/project-view.component.ts
+++ src/app/project-view/project-view.component.ts
@@ -12,6 +12,23 @@ export class ProjectViewComponent implements OnInit {
   userId: Number = -1;
   projectTitle = '';
   newTitle = '';
+  newTaskData: Map<string, string> = new Map<string, string>([
+    ['title', ''],
+    ['description', ''],
+    ['deadline', ''],
+  ]);
+  pages = [0, 1, 2, 3, 4, 5];
+  projectTasks = [
+    {
+      title: 'example',
+      description: '',
+      deadline: '',
+      labels: [],
+      milestoneId: '',
+      statusId: '',
+      assignee: '',
+    },
+  ];
   constructor(
     private http: HttpClient,
     private _router: Router,
@@ -38,10 +55,65 @@ export class ProjectViewComponent implements OnInit {
         console.log(e);
       }
     }
+    this.onGetTasks();
   }
 
   onInput(event: any) {
     this.newTitle = event.target.value;
+  }
+
+  onNewTaskInput(event: any) {
+    this.newTaskData.set(event.target.name, event.target.value);
+    console.log(this.newTaskData);
+  }
+
+  onGetTasks(sort = '', size = 10, page = 1) {
+    try {
+      const headers = new HttpHeaders()
+        .set('content-type', 'application/json')
+        .set('Authorization', `Bearer ${localStorage.getItem('authToken')}`);
+      this.http
+        .get<any>(
+          `/api/project/${this.projectId}/task/${
+            sort && `?sort=${sort}?limit=${size}?page=${page}`
+          }`,
+
+          { headers: headers }
+        )
+        .subscribe((data) => {
+          console.log(data);
+          this.projectTasks = data.tasks;
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  onAddTask() {
+    if (this.newTaskData.get('title')) {
+      try {
+        const headers = new HttpHeaders()
+          .set('content-type', 'application/json')
+          .set('Authorization', `Bearer ${localStorage.getItem('authToken')}`);
+        this.http
+          .post<any>(
+            `/api/project/${this.projectId}/task`,
+            {
+              title: this.newTaskData.get('title'),
+              description: this.newTaskData.get('description'),
+              deadline: this.newTaskData.get('deadline'),
+              projectId: this.projectId,
+            },
+            { headers: headers }
+          )
+          .subscribe((data) => {
+            console.log(data);
+            this.projectTasks = data;
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    }
   }
 
   updateProject() {
